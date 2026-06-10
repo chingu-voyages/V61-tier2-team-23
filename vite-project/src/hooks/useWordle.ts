1469ec433824ae731996e4098f0444266ffb4975
Viann C <46937676+vivchc@users.noreply.github.com>
2026-06-10 01:46:10 -0400

    feat: output message for correct answer and >= 6 attempts

---
+++ vite-project/src/hooks/useWordle.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+export const useWordle = (solution: string) => {
+	const [turn, setTurn] = useState<number>(0); // Tracks current attempt (0 - 5)
+	const [guesses, setGuesses] = useState<string[]>([]); // array of guessed words
+	const [isCorrect, setIsCorrect] = useState<boolean>(false);
+
+	const handleGuess = (currentGuess: string) => {
+		// Adds current guess to end of guesses array
+		const updatedGuesses = guesses.concat(currentGuess);
+		// Update state of guesses array with new array
+		setGuesses(updatedGuesses);
+
+		// Check if the guess matches the secret word
+		if (currentGuess === solution) {
+			setIsCorrect(true);
+			console.log('You guessed the word!');
+			return;
+		}
+
+		// Else, start the next attempt
+		const nextTurn = turn + 1;
+		setTurn(nextTurn);
+
+		// Check if player used all 6 attempts
+		if (nextTurn >= 6) {
+			console.log("Game Over! You\'ve used up all 6 attempts.");
+		}
+	};
+
+	return { turn, guesses, isCorrect, handleGuess };
+};
