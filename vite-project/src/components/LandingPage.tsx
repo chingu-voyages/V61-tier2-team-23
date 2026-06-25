f0c697bbce9db852e7f31d39441c5c49609a99c0
Matthew Neie <matneie@gmail.com>
2026-06-25 08:28:15 -0700

    add signup page, login page, and landing page. also adds react-router

---
+++ vite-project/src/components/LandingPage.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Logo from "../images/Guessifylogo.png";
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-[100vh] bg-[#f3f3f1] flex flex-col text-center items-center">
+      <div className="text-[50px] font-bold mt-8">GUESSIFY</div>
+      <div className="text-[#9da0a1] mt-2">
+        Guess the hidden word. You have 6 tries.
+      </div>
+      <img src={Logo} className="h-40 mt-4"></img>
+      <div className="flex flex-row">
+        <div
+          className="w-[240px] p-3 bg-[#1a1a1b] text-white font-semibold rounded-lg mr-2 hover:cursor-pointer"
+          onClick={() => {
+            navigate("/game");
+          }}
+        >
+          Play now
+        </div>
+        <div
+          className="w-[240px] p-3 bg-[#6aaa64] text-white font-semibold rounded-lg ml-2 hover:cursor-pointer"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Sign in
+        </div>
+      </div>
+      <div className="flex flex-row mt-2 text-[14px]">
+        <p className="text-[#9da0a1] font-semibold mr-1">No account?</p>
+        <p
+          className="text-[#6aaa64] font-semibold ml-1 hover:cursor-pointer"
+          onClick={() => {
+            navigate("/register");
+          }}
+        >
+          Create one free
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ vite-project/src/components/SignUp.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="h-[100vh] bg-[#f3f3f1]">
+      <div className="flex flex-col text-center items-center justify-center">
+        <div className="text-[#9da0a1] mt-10">--- Back to home</div>
+        <div className="text-black font-bold text-[40px] mt-4">
+          Create account
+        </div>
+        <div className="text-[#9da0a1]">
+          Join GUESSIFY and track your streak
+        </div>
+        <div className="w-[500px] p-3 bg-white text-[14px] font-semibold border-1 border-gray-200 rounded-md mt-8 hover:cursor-pointer">
+          Continue with Google
+        </div>
+        <div className="text-gray-400 text-[12px] mt-6">
+          --------------------------------------------------- Or
+          ---------------------------------------------------
+        </div>
+        <div className="w-[500px] flex flex-col text-start mt-6">
+          <div className="">
+            <p className="text-[10px] font-bold">NAME</p>
+            <input
+              type="text"
+              className="w-[494px] bg-white ml-1 mt-2 p-3 px-4 border-1 border-gray-200 rounded-lg placeholder-[#9da0a1] text-sm focus:outline-none"
+              placeholder="Ada Chen"
+            ></input>
+          </div>
+          <div className="mt-4">
+            <p className="text-[10px] font-bold">EMAIL</p>
+            <input
+              type="text"
+              className="w-[494px] bg-white ml-1 mt-2 p-3 px-4 border-1 border-gray-200 rounded-lg placeholder-[#9da0a1] text-sm focus:outline-none"
+              placeholder="adaChen@email.com"
+            ></input>
+          </div>
+          <div className="mt-4">
+            <p className="text-[10px] font-bold">PASSWORD</p>
+            <input
+              type="password"
+              placeholder="8 + characters"
+              className="w-[494px] bg-white ml-1 mt-2 p-3 px-4 border-1 border-gray-200 rounded-lg placeholder-[#9da0a1] text-sm focus:outline-none"
+            ></input>
+          </div>
+          <div className="mt-4">
+            <p className="text-[10px] font-bold">CONFIRM PASSWORD</p>
+            <input
+              type="password"
+              placeholder="8 + characters"
+              className="w-[494px] bg-white ml-1 mt-2 p-3 px-4 border-1 border-gray-200 rounded-lg placeholder-[#9da0a1] text-sm focus:outline-none"
+            ></input>
+          </div>
+        </div>
+        <div className="w-[500px] p-3 bg-[#6aaa64] border-1 rounded-lg text-white font-semibold mt-8 hover:cursor-pointer">
+          Create Account
+        </div>
+        <div className="mt-10 flex text-center items-center justify-center">
+          <p className="w-[200px] text-[12px] text-[#9da0a1]">
+            By signing up you agree to our Terms and Privacy policy
+          </p>
+        </div>
+        <div className="w-[200px] flex flex-row text-center items-center justify-center mt-8">
+          <p className="text-[12px] text-[#9da0a1]">Already have an account?</p>
+          <p className="text-[12px] text-[#6aaa64] ml-2 hover:cursor-pointer">
+            Sign in
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
